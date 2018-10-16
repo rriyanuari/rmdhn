e81d6a1e9f0e486df1af4d81c398997e36462851
rriyanuari <42632781+rriyanuari@users.noreply.github.com>
2018-10-16 10:57:27 +0700

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,45 +1,74 @@
 $(document).ready( function(){
 	
-	// $('#container').load('home.html').hide().fadeIn('slow');
+	
 	$("#container").hide();
 	$("#container").load("home.html", function(){
-            $(this).fadeIn();
+            $(this).fadeIn('slow');
     });
-	$('#lanjutan').hide();
-	$('#aboutMe1').addClass('clicklink');
+	$('#atas').hide();
+	
 	
 	$('#home').click(function(){
 		$("#container").hide();
 		$('#container').load("home.html", function(){
-            $(this).fadeIn();
+            $(this).fadeIn('slow');
     });
-		$('#lanjutan').hide();
-		$('#turunan').show();
-		$('#aboutMe1').addClass('clicklink');
+		$('#atas').hide();
+		$('#bawah').show();
 		$('#home').removeClass('clicklink');
 	});
 
+	
 	$('#aboutMe1').click(function(){
 		$("#container").hide();
 		$('#container').load("aboutMe.html", function(){
-            $(this).fadeIn();
+            $(this).fadeIn('slow');
     });
-		$('#lanjutan').show();
-		$('#turunan').hide();
+		$('#atas').show();
+		$('#aboutMe2').show();
+		$('#contact2').hide();
+		$('#bawah').hide();
 		$('#home').addClass('clicklink');
-		$('#aboutMe1').removeClass('clicklink');
+		
 	});
 
+	
 	$('#aboutMe2').click(function(){
 		$("#container").hide();	
 		$('#container').load("aboutMe.html", function(){
-            $(this).fadeIn();
+            $(this).fadeIn('slow');
     });
-		$('#lanjutan').show();
-		$('#turunan').hide();
+		$('#atas').show();
+		$('#aboutMe2').show();
+		$('#contact2').hide();
+		$('#bawah').hide();
 		$('#home').addClass('clicklink');
-		$('#aboutMe1').removeClass('clicklink');
 	});
+
+	$('#contact1').click(function(){
+		$("#container").hide();	
+		$('#container').load("contact.html", function(){
+            $(this).fadeIn('slow');
+    });
+		$('#atas').show();
+		$('#contact2').show();
+		$('#aboutMe2').hide();
+		$('#bawah').hide();
+		$('#home').addClass('clicklink');
+	});
+
+	$('#contact2').click(function(){
+		$("#container").hide();	
+		$('#container').load("contact.html", function(){
+            $(this).fadeIn('slow');
+    });
+		$('#atas').show();
+		$('#contact2').show();
+		$('#aboutMe2').hide();
+		$('#bawah').hide();
+		$('#home').addClass('clicklink');
+	});
+	
 
 
 });
